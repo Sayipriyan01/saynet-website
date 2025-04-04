ff25541cbfb9a82b1fcc6d798d6524afeb60d893
Sayipriyan01 <sayipriyan2000@gmail.com>
2025-04-04 16:59:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Automatischer Slider
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showNextSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+setInterval(showNextSlide, 4000);
